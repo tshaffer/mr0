4db0baba87752ea3f0ee1f4e1040f098d8bbf9b4
Ted Shaffer <ted@brightsign.biz>
2018-05-05 15:40:44 -0700

    Initial Commit

---
+++ mr0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mr0
+//
+//  Created by Ted Shaffer on 5/5/18.
+//  Copyright © 2018 Ted Shaffer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
