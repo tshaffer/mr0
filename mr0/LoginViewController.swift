8053ca4bab67775ce386f0c7d32a3eecc465572a
Ted Shaffer <ted@brightsign.biz>
2018-05-12 16:38:02 -0700

    added register and login view controllers

---
+++ mr0/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginViewController.swift
+//  mr0
+//
+//  Created by Ted Shaffer on 5/12/18.
+//  Copyright © 2018 Ted Shaffer. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func loginPressed(_ sender: Any) {
+    }
+
+}
---
+++ mr0/RegisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterViewController.swift
+//  mr0
+//
+//  Created by Ted Shaffer on 5/12/18.
+//  Copyright © 2018 Ted Shaffer. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func registerPressed(_ sender: Any) {
+    }
+}
