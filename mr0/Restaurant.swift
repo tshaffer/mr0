50e348508de0392d6123d3606719f150f44af9ab
Ted Shaffer <ted@brightsign.biz>
2018-05-12 15:50:58 -0700

    added home screen for register / login.

---
+++ mr0/Restaurant.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant.swift
+//  mr0
+//
+//  Created by Ted Shaffer on 5/12/18.
+//  Copyright © 2018 Ted Shaffer. All rights reserved.
+//
+
+import Foundation
+
+class Restaurant {
+    
+    var name : String = ""
+    var foodType : String = ""
+    var comments : String = ""
+    var dateVisited = Date()
+}
