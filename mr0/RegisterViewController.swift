d72237578b58f8c710ef4b7f1a31111da90d65bc
Ted Shaffer <ted@brightsign.biz>
2018-05-12 16:44:54 -0700

    register a new user

--- mr0/RegisterViewController.swift
+++ mr0/RegisterViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import SVProgressHUD
 
 class RegisterViewController: UIViewController {
 
@@ -26,5 +28,23 @@ class RegisterViewController: UIViewController {
     
 
     @IBAction func registerPressed(_ sender: Any) {
+        
+        SVProgressHUD.show()
+        
+        //Set up a new user on our Firebase database
+        
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            
+            if error != nil {
+                print(error!)
+            } else {
+                print("Registration Successful!")
+                
+//                SVProgressHUD.dismiss()
+                
+//                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
+    
     }
 }
