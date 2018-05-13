898926104e114737c617c3af8df339eb2714d073
Ted Shaffer <ted@brightsign.biz>
2018-05-13 06:01:53 -0700

    added Login functionality

--- mr0/LoginViewController.swift
+++ mr0/LoginViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import SVProgressHUD
 
 class LoginViewController: UIViewController {
 
@@ -26,6 +28,20 @@ class LoginViewController: UIViewController {
     
 
     @IBAction func loginPressed(_ sender: Any) {
+        SVProgressHUD.show()
+        
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            
+            if error != nil {
+                print(error!)
+            } else {
+                print("Log in successful!")
+                
+                SVProgressHUD.dismiss()
+                
+                self.performSegue(withIdentifier: "goToMemoRapp", sender: self)
+                
+            }
+        }
     }
-
 }
