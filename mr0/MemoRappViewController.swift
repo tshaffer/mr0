c3a6caeb884f7600f5b342e9e0882f6c246307b4
Ted Shaffer <ted@brightsign.biz>
2018-05-20 15:30:56 -0700

    demonstration of reading from db and json serialization

--- mr0/MemoRappViewController.swift
+++ mr0/MemoRappViewController.swift
@@ -26,6 +26,63 @@ class MemoRappViewController: UITableViewController {
     override func viewDidLoad() {
         print ("MemoRappViewController viewDidLoad")
         super.viewDidLoad()
+        
+        let restaurantsDB = Database.database().reference().child("Restaurants")
+        restaurantsDB.observe(.childAdded, with: { (snapshot) in
+            if let getData = snapshot.value as? [String:Any] {
+                
+                print(getData)
+                
+                let sender = getData["Sender"] as? String
+                print(sender ?? "senderPoo")
+                
+                let restaurantBody = getData["RestaurantBody"] as? String
+                print(restaurantBody ?? "restaurantBodyPoo")
+                
+                let jsonData = restaurantBody?.data(using: .utf8)
+                let dictionary = try? JSONSerialization.jsonObject(with: jsonData!, options: .mutableLeaves)
+                print(dictionary!)
+                let aD = dictionary as! Dictionary<String, AnyObject>
+                
+                let name = aD["name"]
+                print(name as Any)
+                print(name!)
+                let comments = aD["comments"]
+                print(comments as Any)
+                print(comments!)
+                
+//                let wins = getData["wins"] as? String
+                
+//                let jsonString = "{\"nacho\":[\"1\",\"2\",\"3\"]}"
+//                let jsonData = jsonString.data(using: .utf8)
+//                let dictionary = try? JSONSerialization.jsonObject(with: jsonData!, options: .mutableLeaves)
+//                print(dictionary!)
+                
+//                let encoder = JSONEncoder()
+//                encoder.outputFormatting = .prettyPrinted
+//
+//                do {
+//                    let data = try encoder.encode(getData)
+//                    print(String(data: data, encoding: .utf8)!)
+//                    let dataAsString = String(data: data, encoding: .utf8)!
+//                    print(dataAsString)
+//                    let restaurantDictionary = ["Sender": Auth.auth().currentUser?.email ?? "ted@roku.com",
+//                                                "RestaurantBody": dataAsString] as [String : Any]
+//                    restaurantsDB.childByAutoId().setValue(restaurantDictionary) {
+//                        (error, reference) in
+//                        if (error != nil) {
+//                            print(error!)
+//                        }
+//                        else {
+//                            print("Restaurant saved successfully")
+//                        }
+//                    }
+//                }
+//                catch {
+//                    print("encoder error")
+//                }
+            }
+        })
     }
 
     @IBAction func logOutPressed(_ sender: Any) {
