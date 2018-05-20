11f886bfe811e6bad876d444851a47bcc016fdb1
Ted Shaffer <ted@brightsign.biz>
2018-05-20 06:51:52 -0700

    move location code from MemoRapp to Restaurant. Display location when user clicks on in view controller.

--- mr0/LocationTableViewController.swift
+++ mr0/LocationTableViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import GooglePlaces
 
 class LocationTableViewController: UITableViewController {
 
+    // An array to hold the list of possible locations.
+    var likelyPlaces: [GMSPlace] = []
+    var selectedPlace: GMSPlace?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,60 +29,20 @@ class LocationTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return likelyPlaces.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell", for: indexPath)
+        let collectionItem = likelyPlaces[indexPath.row]
+        cell.textLabel?.text = collectionItem.name
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+    
     /*
     // MARK: - Navigation
 
--- mr0/RestaurantReviewViewController.swift
+++ mr0/RestaurantReviewViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import GooglePlaces
 
 class RestaurantReviewViewController: UIViewController, SpecifyFoodTypeDelegate {
 
@@ -18,9 +19,39 @@ class RestaurantReviewViewController: UIViewController, SpecifyFoodTypeDelegate 
     
     var foodType: FoodType = .Other
     
+    var locationManager = CLLocationManager()
+    var currentLocation: CLLocation?
+    var placesClient: GMSPlacesClient!
+    
+    // An array to hold the list of likely places.
+    var likelyPlaces: [GMSPlace] = []
+    
+    // The currently selected place.
+    var selectedPlace: GMSPlace?
+    
+    // A default location to use when location permission is not granted.
+    let defaultLocation = CLLocation(latitude: -33.869405, longitude: 151.199)
+    
+    // Update the map once the user has made their selection.
+    @IBAction func unwindToMain(segue: UIStoryboardSegue) {
+        listLikelyPlaces()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
+        
+        // Initialize the location manager.
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.distanceFilter = 50
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+        
+        placesClient = GMSPlacesClient.shared()
+        
+        listLikelyPlaces()
     }
 
     override func didReceiveMemoryWarning() {
@@ -42,6 +73,30 @@ class RestaurantReviewViewController: UIViewController, SpecifyFoodTypeDelegate 
         }
     }
     
+    // Populate the array with the list of likely places.
+    func listLikelyPlaces() {
+        // Clean up from previous sessions.
+        likelyPlaces.removeAll()
+        
+        placesClient.currentPlace(callback: { (placeLikelihoods, error) -> Void in
+            if let error = error {
+                // TODO: Handle the error.
+                print("Current Place error: \(error.localizedDescription)")
+                return
+            }
+            
+            // Get likely places and add to the list.
+            if let likelihoodList = placeLikelihoods {
+                for likelihood in likelihoodList.likelihoods {
+                    let place = likelihood.place
+                    print("place: \(place)")
+                    self.likelyPlaces.append(place)
+                }
+            }
+        })
+    }
+    
+
     @IBAction func submit(_ sender: UIButton) {
         print("button pressed")
         print(restaurantName.text!)
@@ -98,7 +153,41 @@ class RestaurantReviewViewController: UIViewController, SpecifyFoodTypeDelegate 
             let vc = segue.destination as! FoodTypeTableViewController
             vc.specifyFoodTypeDelegate = self
         }
+        else if (segue.identifier == "specifyLocationSegue") {
+            print("segue to LocationTableViewController")
+            if let nextViewController = segue.destination as? LocationTableViewController {
+                nextViewController.likelyPlaces = likelyPlaces
+            }
+
+        }
+        
     }
+}
 
 
+// Delegates to handle events for the location manager.
+extension RestaurantReviewViewController: CLLocationManagerDelegate {
+    
+    // Handle incoming location events.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location: CLLocation = locations.last!
+        print("Location: \(location)")
+        
+        listLikelyPlaces()
+    }
+    
+    // Handle authorization for the location manager.
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .restricted:
+            print("Location access was restricted.")
+        case .denied:
+            print("User denied access to location.")
+        case .notDetermined:
+            print("Location status not determined.")
+        case .authorizedAlways: fallthrough
+        case .authorizedWhenInUse:
+            print("Location status is OK.")
+        }
+    }
 }
