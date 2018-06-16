05684c8100249fb2c2e499ce7e191eba596fb413
Ted Shaffer <ted@brightsign.biz>
2018-06-16 06:38:43 -0700

    toggle check marks by tag when selected

--- mr0/TagsViewController.swift
+++ mr0/TagsViewController.swift
@@ -40,12 +40,12 @@ class TagsViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-//        let cells = tableView.visibleCells
-//
-//        for cell in cells {
-//            cell.accessoryType = .none
-//        }
-        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        if (tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark) {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
         
         tableView.deselectRow(at: indexPath, animated: true)
         
