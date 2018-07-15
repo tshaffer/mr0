6e68d939c4709eaf9b1ae143b65f41b3af82cc7a
Ted Shaffer <ted@brightsign.biz>
2018-07-15 06:03:06 -0700

    another one that didn't work. creating xib, then class.

---
+++ mr0/Views/MenuItemCustomCell/CustomMenuItemCell.swift
@@ -0,0 +1,20 @@
+//
+//  CustomMenuItemCell.swift
+//  mr0
+//
+//  Created by Ted Shaffer on 7/15/18.
+//  Copyright © 2018 Ted Shaffer. All rights reserved.
+//
+
+import UIKit
+
+class CustomMenuItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+
+}
